6d0800eb47933b8bba961e86be20ba8fc197550a
Paulo César de Carvalho <paulocesarcarvalho@rocketmail.com>
2022-10-17 19:24:20 -0300

    1

--- Caça-palavras/dados.js
+++ Caça-palavras/dados.js
@@ -1,4 +1,4 @@
-const palavrasListadas =["Brasi","Argentina","Uruguai","Paraguai"]
+const palavrasListadas = ["Brasil","Argentina","Uruguai","Paraguai","Alemanha","Itália","França"]
 
 const nColunas = 12
 const nLinhas = 15
--- Caça-palavras/principal.js
+++ Caça-palavras/principal.js
@@ -1,5 +1,5 @@
 function aleatorio_entre(inf, sup){
-    return Math.floor(Math.random()*(sup - inf +1) + inf)
+    return Math.floor(Math.random()*(sup - inf + 1) + inf)
 }
 
 
@@ -36,25 +36,29 @@ function montar_array (numColunas, numLinhas){
     return arrayCompleto
 }
 
-
 let arrayCompleto = montar_array(nColunas,nLinhas)
-let palavra = "Brasil".toUpperCase()
-let nLetra = palavra.length
 
+for (let p=0 ; p < palavrasListadas.length ;p++){
+    
+    let palavra = palavrasListadas[p].toUpperCase()
+    let nLetra = palavra.length
 
+    //posicionamento vertical
+    let linhaInicialPalavra = aleatorio_entre(0,nLinhas-1)
+    let colunaInicialPalavra = aleatorio_entre(0, nColunas-nLetra)
 
+    //Impressão da palavra quando possível for:
+    for (let i = 0;i<nLetra;i++){
+        arrayCompleto[linhaInicialPalavra][colunaInicialPalavra+i]=palavra[i]
+    }
 
-//posicionamento vertical
-let linhaInicialPalavra = aleatorio_entre(1,nLinhas)
-let colunaInicialPalavra = aleatorio_entre(1, nColunas-nLetra)
-for (let i = 0;i<nLetra;i++){
-    arrayCompleto[linhaInicialPalavra][colunaInicialPalavra+i]=palavra[i]
 }
-
-console.log(linhaInicialPalavra+1,colunaInicialPalavra+1)
-
-for(i = 0; i<100;i++){
-    console.log(aleatorio_entre(65,90))
-}
-
+//console.log(arrayCompleto)
 montar_diagrama(arrayCompleto)
+
+// console.log(linhaInicialPalavra+1,colunaInicialPalavra+1)
+
+// for(i = 0; i<100;i++){
+//     console.log(aleatorio_entre(65,90))
+// }
+
