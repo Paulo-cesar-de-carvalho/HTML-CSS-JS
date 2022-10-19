3a8df5c08cc74043ad6bf011eb28737344ae1de7
Paulo César de Carvalho <paulocesarcarvalho@rocketmail.com>
2022-10-19 12:20:37 -0300

    1

--- Caça-palavras/dados.js
+++ Caça-palavras/dados.js
@@ -1,7 +1,15 @@
-const palavrasListadas = ["Brasil","Argentina","Uruguai","Paraguai","Alemanha","Itália","França","Peru","Holanda","Noruega","Canadá","Suécia","Inglaterra"]
+const palavrasListadas = ["Brasil","Argentina","Uruguai","Paraguai","Alemanha","Itália","França","Peru","Holanda","Noruega","Canadá","Suécia","Inglaterra","Dinamarca",
+    "Polônia","Hungria"]
 
 const letras = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
+const direcoes = ["horizontal","vertical","diagonalPrincipal", "diagonalSecundária", "horizontalInvertida",
+    "verticalSubindo","diagonalPrincialinvertida", "diagonalSecundáriaInvertida"]
 
-const nColunas = 18
-const nLinhas = 25
+
+const nColunas = 25
+const nLinhas = 36
+const tentativas = 50
+let dificuldade = 8
+
+
--- Caça-palavras/principal.js
+++ Caça-palavras/principal.js
@@ -36,100 +36,210 @@ function montar_array (numColunas, numLinhas){
     return arrayCompleto
 }
 
-let arrayCompleto = montar_array(nColunas,nLinhas)
-let tentativas = 50
-let incluidas = 0
-let direcao = 0 // 1-horizontal; 2-vertica; 3-diagonal descendo
-let dificuldade = 3
-
-
-
-
-//cada palavra
-for (let p = 0 ; p < palavrasListadas.length ;p++){
-    
-    let palavra = palavrasListadas[p].toUpperCase()
+function encaixar_palavra(palavra){
+    palavra = palavra.toUpperCase()
     let nLetra = palavra.length
-    direcao = aleatorio_entre(1,dificuldade)
-
-    //posicionamento horizontal
-    if (direcao == 1){
-        for (let t = 0; t < tentativas;t++){
-            let linhaInicialPalavra = aleatorio_entre(0,nLinhas-1)
-            let colunaInicialPalavra = aleatorio_entre(0, nColunas-nLetra)
-            // Testar se é possível escrever neste lugar
-            let espacoVazio = false
-            for (let i = 0; i < nLetra ; i++){
-                if (arrayCompleto[linhaInicialPalavra][colunaInicialPalavra+i] == "" || arrayCompleto[linhaInicialPalavra][colunaInicialPalavra+i] == palavra[i] ){
-                    espacoVazio = true
-                } else{
-                    espacoVazio = false
+    direcao = aleatorio_entre(0,dificuldade-1)
+    switch (direcoes[direcao]){
+        case "horizontal" :  
+            for (let t = 0; t < tentativas;t++){
+                let linhaInicialPalavra = aleatorio_entre(0,nLinhas-1)
+                let colunaInicialPalavra = aleatorio_entre(0, nColunas-nLetra)
+                // Testar se é possível escrever neste lugar
+                let espacoVazio = false
+                for (let i = 0; i < nLetra ; i++){
+                    if (arrayCompleto[linhaInicialPalavra][colunaInicialPalavra+i] == "" || arrayCompleto[linhaInicialPalavra][colunaInicialPalavra+i] == palavra[i] ){
+                        espacoVazio = true
+                    } else{
+                        espacoVazio = false
+                        break
+                    }
+                }
+                //Impressão da palavra quando possível for:
+                if (espacoVazio){
+                    for (let i = 0;i<nLetra;i++){
+                        arrayCompleto[linhaInicialPalavra][colunaInicialPalavra+i]=palavra[i]
+                    }
+                    incluidas ++
                     break
                 }
             }
-            //Impressão da palavra quando possível for:
-            if (espacoVazio){
-                for (let i = 0;i<nLetra;i++){
-                    arrayCompleto[linhaInicialPalavra][colunaInicialPalavra+i]=palavra[i]
+            break
+        case "vertical" : 
+            for (let t = 0; t < tentativas;t++){
+                let linhaInicialPalavra = aleatorio_entre(0,nLinhas-nLetra)
+                let colunaInicialPalavra = aleatorio_entre(0, nColunas-1)
+                // Testar se é possível escrever neste lugar
+                let espacoVazio = false
+                for (let i = 0; i < nLetra ; i++){
+                    if (arrayCompleto[linhaInicialPalavra+i][colunaInicialPalavra] == "" || arrayCompleto[linhaInicialPalavra+i][colunaInicialPalavra] == palavra[i] ){
+                        espacoVazio = true
+                    } else{
+                        espacoVazio = false
+                        break
+                    }
                 }
-                incluidas ++
-                break
+                //Impressão da palavra quando possível for:
+                if (espacoVazio){
+                    for (let i = 0;i<nLetra;i++){
+                        arrayCompleto[linhaInicialPalavra + i][colunaInicialPalavra]=palavra[i]
+                    }
+                    incluidas ++
+                    break
+                }
             }
-        }
-    }
+            break
+        case "diagonalPrincipal" :  
+            for (let t = 0; t < tentativas;t++){
+                let linhaInicialPalavra = aleatorio_entre(0,nLinhas-nLetra)
+                let colunaInicialPalavra = aleatorio_entre(0, nColunas-nLetra)
+                // Testar se é possível escrever neste lugar
+                let espacoVazio = false
+                for (let i = 0; i < nLetra ; i++){
+                    if (arrayCompleto[linhaInicialPalavra+i][colunaInicialPalavra+i] == "" || arrayCompleto[linhaInicialPalavra+i][colunaInicialPalavra+i] == palavra[i] ){
+                        espacoVazio = true
+                    } else{
+                        espacoVazio = false
+                        break
+                    }
+                }
+                //Impressão da palavra quando possível for:
+                if (espacoVazio){
+                    for (let i = 0;i<nLetra;i++){
+                        arrayCompleto[linhaInicialPalavra + i][colunaInicialPalavra + i]=palavra[i]
+                    }
+                    incluidas ++
+                    break
+                }
+            }
+            break
+        case "diagonalSecundária" :  
+            for (let t = 0; t < tentativas;t++){
+                let linhaInicialPalavra = aleatorio_entre(nLetra-1,nLinhas-1)
+                let colunaInicialPalavra = aleatorio_entre(0, nColunas-nLetra)
+                // Testar se é possível escrever neste lugar
+                let espacoVazio = false
+                for (let i = 0; i < nLetra ; i++){
+                    if (arrayCompleto[linhaInicialPalavra-i][colunaInicialPalavra+i] == "" || arrayCompleto[linhaInicialPalavra-i][colunaInicialPalavra+i] == palavra[i] ){
+                        espacoVazio = true
+                    } else{
+                        espacoVazio = false
+                        break
+                    }
+                }
+                //Impressão da palavra quando possível for:
+                if (espacoVazio){
+                    for (let i = 0;i<nLetra;i++){
+                        arrayCompleto[linhaInicialPalavra - i][colunaInicialPalavra + i]=palavra[i]
+                    }
+                    incluidas ++
+                    break
+                }
+            }
+            break
+        case "horizontalInvertida" :  
+            for (let t = 0; t < tentativas;t++){
+                let linhaInicialPalavra = aleatorio_entre(0,nLinhas-1)
+                let colunaInicialPalavra = aleatorio_entre(nLetra-1, nColunas-1)
+                // Testar se é possível escrever neste lugar
+                let espacoVazio = false
+                for (let i = 0; i < nLetra ; i++){
+                    if (arrayCompleto[linhaInicialPalavra][colunaInicialPalavra-i] == "" || arrayCompleto[linhaInicialPalavra][colunaInicialPalavra-i] == palavra[i] ){
+                        espacoVazio = true
+                    } else{
+                        espacoVazio = false
+                        break
+                    }
+                }
+                //Impressão da palavra quando possível for:
+                if (espacoVazio){
+                    for (let i = 0;i<nLetra;i++){
+                        arrayCompleto[linhaInicialPalavra][colunaInicialPalavra-i]=palavra[i]
+                    }
+                    incluidas ++
+                    break
+                }
+            }
+            break
+        case "verticalSubindo" : 
+            for (let t = 0; t < tentativas;t++){
+                let linhaInicialPalavra = aleatorio_entre(nLetra-1,nLinhas-1)
+                let colunaInicialPalavra = aleatorio_entre(0, nColunas-1)
+                // Testar se é possível escrever neste lugar
+                let espacoVazio = false
+                for (let i = 0; i < nLetra ; i++){
+                    if (arrayCompleto[linhaInicialPalavra-i][colunaInicialPalavra] == "" || arrayCompleto[linhaInicialPalavra-i][colunaInicialPalavra] == palavra[i] ){
+                        espacoVazio = true
+                    } else{
+                        espacoVazio = false
+                        break
+                    }
+                }
+                //Impressão da palavra quando possível for:
+                if (espacoVazio){
+                    for (let i = 0;i<nLetra;i++){
+                        arrayCompleto[linhaInicialPalavra - i][colunaInicialPalavra]=palavra[i]
+                    }
+                    incluidas ++
+                    break
+                }
+            }
+            break
+        case "diagonalPrincialinvertida" :  
+            for (let t = 0; t < tentativas;t++){
+                let linhaInicialPalavra = aleatorio_entre(nLetra-1,nLinhas-1)
+                let colunaInicialPalavra = aleatorio_entre(nLetra-1, nColunas-1)
+                // Testar se é possível escrever neste lugar
+                let espacoVazio = false
+                for (let i = 0; i < nLetra ; i++){
+                    if (arrayCompleto[linhaInicialPalavra-i][colunaInicialPalavra-i] == "" || arrayCompleto[linhaInicialPalavra-i][colunaInicialPalavra-i] == palavra[i] ){
+                        espacoVazio = true
+                    } else{
+                        espacoVazio = false
+                        break
+                    }
+                }
+                //Impressão da palavra quando possível for:
+                if (espacoVazio){
+                    for (let i = 0;i<nLetra;i++){
+                        arrayCompleto[linhaInicialPalavra - i][colunaInicialPalavra - i]=palavra[i]
+                    }
+                    incluidas ++
+                    break
+                }
+            }
+            break
+        case "diagonalSecundáriaInvertida" :  
+            for (let t = 0; t < tentativas;t++){
+                let linhaInicialPalavra = aleatorio_entre(0,nLinhas-nLetra)
+                let colunaInicialPalavra = aleatorio_entre(nLetra-1, nColunas-1)
+                // Testar se é possível escrever neste lugar
+                let espacoVazio = false
+                for (let i = 0; i < nLetra ; i++){
+                    if (arrayCompleto[linhaInicialPalavra+i][colunaInicialPalavra-i] == "" || arrayCompleto[linhaInicialPalavra+i][colunaInicialPalavra-i] == palavra[i] ){
+                        espacoVazio = true
+                    } else{
+                        espacoVazio = false
+                        break
+                    }
+                }
+                //Impressão da palavra quando possível for:
+                if (espacoVazio){
+                    for (let i = 0;i<nLetra;i++){
+                        arrayCompleto[linhaInicialPalavra + i][colunaInicialPalavra - i]=palavra[i]
+                    }
+                    incluidas ++
+                    break
+                }
+            }
+            break
+        default :
+            console.log("nenhum")
     
-    //posicionamento vertical
-    if (direcao == 2){
-        for (let t = 0; t < tentativas;t++){
-            let linhaInicialPalavra = aleatorio_entre(0,nLinhas-nLetra)
-            let colunaInicialPalavra = aleatorio_entre(0, nColunas-1)
-            // Testar se é possível escrever neste lugar
-            let espacoVazio = false
-            for (let i = 0; i < nLetra ; i++){
-                if (arrayCompleto[linhaInicialPalavra+i][colunaInicialPalavra] == "" || arrayCompleto[linhaInicialPalavra+i][colunaInicialPalavra] == palavra[i] ){
-                    espacoVazio = true
-                } else{
-                    espacoVazio = false
-                    break
-                }
-            }
-            //Impressão da palavra quando possível for:
-            if (espacoVazio){
-                for (let i = 0;i<nLetra;i++){
-                    arrayCompleto[linhaInicialPalavra + i][colunaInicialPalavra]=palavra[i]
-                }
-                incluidas ++
-                break
-            }
-        }
-    }
-    //posicionamento diagonal
-    if (direcao == 3){
-        for (let t = 0; t < tentativas;t++){
-            let linhaInicialPalavra = aleatorio_entre(0,nLinhas-nLetra)
-            let colunaInicialPalavra = aleatorio_entre(0, nColunas-nLetra)
-            // Testar se é possível escrever neste lugar
-            let espacoVazio = false
-            for (let i = 0; i < nLetra ; i++){
-                if (arrayCompleto[linhaInicialPalavra+i][colunaInicialPalavra+i] == "" || arrayCompleto[linhaInicialPalavra+i][colunaInicialPalavra+i] == palavra[i] ){
-                    espacoVazio = true
-                } else{
-                    espacoVazio = false
-                    break
-                }
-            }
-            //Impressão da palavra quando possível for:
-            if (espacoVazio){
-                for (let i = 0;i<nLetra;i++){
-                    arrayCompleto[linhaInicialPalavra + i][colunaInicialPalavra + i]=palavra[i]
-                }
-                incluidas ++
-                break
-            }
-        }
     }
 
 }
+
 function preencher_vazias (arrayComVazias){
     for (i=0; i<arrayComVazias.length;i++){
         for(a=0; a<arrayComVazias[0].length; a++){
@@ -138,6 +248,17 @@ function preencher_vazias (arrayComVazias){
         }
     }
     return arrayComVazias
+}
+
+
+let arrayCompleto = montar_array(nColunas,nLinhas)
+
+let incluidas = 0
+let direcao = 0 
+
+//cada palavra
+for (palavra of palavrasListadas){ 
+    encaixar_palavra(palavra)
 }
 
 preencher_vazias(arrayCompleto)
@@ -152,3 +273,15 @@ montar_diagrama(arrayCompleto)
 //     console.log(aleatorio_entre(65,90))
 // }
 
+let listaPalavras = document.querySelector("#palavras")
+
+function listar_palavras (palavra){
+    let liPalavra = document.createElement("li")
+    liPalavra.innerText = palavra
+    listaPalavras.appendChild(liPalavra)
+}
+
+palavrasListadas.sort()
+for (i=0;i<palavrasListadas.length;i++){
+    listar_palavras(palavrasListadas[i])
+}
