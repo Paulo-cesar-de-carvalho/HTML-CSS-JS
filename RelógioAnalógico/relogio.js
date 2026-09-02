3925c88998468f33ab050e88270e2a6dcd61ed38
Paulo César de Carvalho <paulocesarcarvalho@rocketmail.com>
2026-09-01 22:16:56 -0300

    1
    1

--- RelógioAnalógico/relogio.js
+++ RelógioAnalógico/relogio.js
@@ -1,11 +1,7 @@
 const mostrador = document.querySelector("#mostrador")
-mostrador.innerText = "mostrador aqui"
 
 const ponteiro = document.createElement('div')
 ponteiro.setAttribute('class','ponteiro')
-ponteiro.innerText = "Ponteiro"
-//ponteiro
-//mostrador.appendChild(ponteiro)
 
 const segundosDoDia = () =>{
     const tempo = new Date
@@ -16,14 +12,6 @@ const segundosDoDia = () =>{
 
 } 
 
-
-// console.log(horas,minutos,segundos, segundosDoDia)
-
-mostrador.addEventListener('click', (ev)=>{
-    console.log( ev.target)
-
-})
-
 class Ponteiro {
     //periodos para ponteiros: segundos 1m; minutos 60m, horas 12*60m 
     constructor(periodoM, comprimento, largura,elemMostrador){
@@ -33,10 +21,8 @@ class Ponteiro {
         this.mostrador = elemMostrador
         this.id = `i${periodoM}`
         this.criacao = this.criar() //funcão é chamada quando se cria um novo objeto da classe
-        
         this.eu = document.getElementById(this.id)
-        //console.log(this.eu)
-        this.giro = setInterval(this.girar, 100)
+        this.giro = setInterval(this.girar, 200)
     }
     criar = ()=>{
         const pont = document.createElement('div')
@@ -44,20 +30,43 @@ class Ponteiro {
         pont.setAttribute('id',this.id)
         this.mostrador.appendChild(pont)
         const angulo = (segundosDoDia()/ this.periodo * 6 - 90) % 360
-        console.log(angulo, this.periodo)
         pont.setAttribute('style',`rotate:${angulo}deg;width:${this.comprimento}px;height:${this.largura}px`)
     }
     girar = ()=>{
         const angulo = (segundosDoDia()/ this.periodo * 6 - 90) % 360
-        //console.log(angulo)
         const pont1 = this.eu
-        //console.log(pont1)
         pont1.setAttribute('style',`rotate:${angulo}deg;width:${this.comprimento}px;height:${this.largura}px`)
 
     }
 
 }
-const ponteiroS = new Ponteiro(1,160,2,mostrador)
-const ponteiroM = new Ponteiro(60,130,4,mostrador)
-const ponteiroH = new Ponteiro(12*60,110,10,mostrador)
+const ponteiroS = new Ponteiro(1,155,2,mostrador)
+const ponteiroM = new Ponteiro(60,140,4,mostrador)
+const ponteiroH = new Ponteiro(12*60,90,10,mostrador)
 
+class Algarismos {
+    constructor(mostrador, algarismo) {
+        this.mostrador = mostrador
+        this.algarismo = algarismo
+        this.id = `a${algarismo}`
+        this.criacao = this.criar()
+        this.eu = document.getElementById(this.id)
+        this.posicao = this.posicionar()
+    }
+    criar = ()=>{
+        const alg = document.createElement('div')
+        alg.setAttribute('class','algarismo')
+        alg.setAttribute('id',this.id)
+        alg.innerText = this.algarismo  
+        this.mostrador.appendChild(alg)
+    }
+    posicionar = ()=>{
+        const tamPoneiro = 180
+        let posicaoX =  200 + tamPoneiro * Math.cos((this.algarismo * 30 - 90) * Math.PI / 180) - this.eu.offsetWidth / 2
+        let posicaoY =  200 + tamPoneiro * Math.sin((this.algarismo * 30 - 90) * Math.PI / 180) -   this.eu.offsetHeight / 2
+        this.eu.setAttribute('style',`left:${posicaoX}px;top:${posicaoY}px`)
+    }
+}
+for (let i = 1; i <= 12; i++) {
+    const algarismo = new Algarismos(mostrador, i)
+}
